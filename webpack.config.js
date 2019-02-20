5858aa63184b2e635025c4012ee1ec7838ed6065
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2019-02-20 18:09:05 +0530

    updated code with error

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  mode: 'production',
+  devtool: 'cheap-module-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: ''
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {loader: 'style-loader'},
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]_[local]_[hash:base64:5]'
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({
+                  "browsers": [
+                    "> 1%",
+                    "last 2 versions"
+                  ]
+                })
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+      }
+    ]
+  }
+};
